6dcacaf546bfb1736c97ff4b852f39da62078338
Joshua Dale <jdale9930@gmail.com>
2021-03-04 16:48:10 -0600

    set up users.model

--- models/users.model.js
+++ models/users.model.js
@@ -1,3 +1,83 @@
 const bcrypt = require("bcrypt")
 const pool = require("../config/mysql.conf")
 
+function isInvalid(val, min, max)
+{
+    return !val || val.length < min || val.length > max;
+}
+
+async function signup(res, username, password)
+{
+    try{
+        const encrypted = await bcrypt.hash(password, 8)
+        if(isInvalid(username, 4, 20) || isInvalid(password, 8, 64))
+        {
+            throw "Invalid Data Provided"
+        }
+    
+
+        let [user] =
+        await pool.query("SELECT * FROM users WHERE users.username = ?", [username])
+        
+        if(user.length > 0)
+        {
+            throw "Username is already taken"
+        }
+
+        await poolquery("INSERT INTO users (username, password) values (?,?"), [
+            username,
+            encrypted
+        ]
+        return res.send({
+            success: true,
+            data: "Succesfully signed up",
+            error: null
+        })
+
+    }
+
+    catch(err){
+        console.log(err)
+        return res.send({
+            success: false,
+            data: null,
+            error: err
+        })
+    }
+}
+
+async function login(res, username, password)
+{
+    try{
+        if(isInvalid(username, 4, 20) || isInvalid(password, 8, 64))
+        {
+            throw "Invalid Data Provided"
+        }
+
+        let [users] = await pool.query("SELECT * FROM users WHERE users.username = ?", [username])
+        if(users.length === 0){
+            throw "Invalid username or password"
+        }
+
+        const match = await bcrypt.compare(password, users[0].password)
+        if (!match) {
+            throw "Invalid username or password"
+        }
+
+        res.send({
+            success: true,
+            data: {username: users[0].username},
+            error: null
+        })    
+    }
+    catch(err){
+        console.log(err)
+        return res.send({
+            success: false,
+            data: null,
+            error: err
+        })
+    }
+}
+
+module.exports = {signup, login}
